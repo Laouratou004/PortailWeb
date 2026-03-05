9ba4e0ddc22f946c8d747cb04291aed68f0e664b
Hadji <hadjiratoudiallo394@gmail.com>
2026-03-05 15:49:43 +0000

    createapp

--- backend/portail_backend/settings.py
+++ backend/portail_backend/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'domains',
+    'links',
+    'ads',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
